9c0e4eeaa50daf431af27e07269590518b1c74f1
Nguyen Minh Tri <33139373+MachiCafe@users.noreply.github.com>
2017-12-08 11:58:22 +0900

    Java GUI test: JCheckBox

---
+++ TestCheckBox.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import java.awt.Container;
+
+
+public class TestCheckBox extends JFrame{
+	public static void main(String[] args){
+		TestCheckBox frame = new TestCheckBox("HELLO");
+		frame.setVisible(true);
+	}
+
+	TestCheckBox(String title){
+			setTitle(title);
+			setSize(500, 400);
+			setLocation(400, 300);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+			JCheckBox check1 = new JCheckBox("Linux");
+			JCheckBox check2 = new JCheckBox("MacOC");
+			JCheckBox check3 = new JCheckBox("ANdroid");
+
+			//Panel 
+			JPanel pnl = new JPanel();
+
+			pnl.add(check1);
+			pnl.add(check2);
+			pnl.add(check3);
+
+			Container cont = getContentPane();
+			cont.add(pnl);
+		}
+}
--- TestJPasswordField.java
+++ TestJPasswordField.java
@@ -1,6 +1,8 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 import java.awt.Container;
 import java.awt.BorderLayout;
 
@@ -11,16 +13,21 @@ public class TestJPasswordField extends JFrame{
 	}
 	TestJPasswordField(String title){
 		setTitle(title);
-		setSize(400,300);
+		setSize(700,600);
 		setLocation(400,300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		//Panel
 		JPanel pnl = new JPanel();
+		JPanel pnl2 = new JPanel();
+		//Label
+		JLabel lbl1 = new JLabel("Email");
+		JLabel lbl2 = new JLabel("Password");
 		//PasswordField
 		JPasswordField pass = new JPasswordField(20);
 
-		pnl.add(pass);
+		pnl.add(lbl1);
+		pnl2.add(pass);
 
 		Container cont = getContentPane();
 		cont.add(pnl, BorderLayout.CENTER);
